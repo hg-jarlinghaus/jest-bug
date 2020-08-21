ea5b92dd6f2e9c784d6ea6ce608e1705bccb5223
John Aringhaus <jarlinghaus@healthguardsecurity.com>
2020-08-21 11:18:27 -0400

    Changed work.test.js to have the same timings as fail.test.js

--- work.test.js
+++ work.test.js
@@ -6,7 +6,7 @@ describe("Working tests", () => {
           console.log(1);
           expect(1).toBe(1);
           aa();
-        }, 2000);
+        }, 1000);
       }),
     ]).then(() => {
       cb();
@@ -19,7 +19,7 @@ describe("Working tests", () => {
           console.log(2);
           expect(2).toBe(2);
           aa();
-        }, 1000);
+        }, 2000);
       }),
     ]).then(() => {
       cb();
